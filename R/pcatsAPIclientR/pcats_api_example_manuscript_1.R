b21e7a141aa5e45648c9376fced20f94be6739db
chen-chen130 <chen_chen130@hotmail.com>
2023-06-15 12:51:58 -0400

    Update pcats_api_example_manuscript_1.R

--- R/pcatsAPIclientR/pcats_api_example_manuscript_1.R
+++ R/pcatsAPIclientR/pcats_api_example_manuscript_1.R
@@ -3,15 +3,16 @@ library(pcatsAPIclientR)
 #example 1
 
 jobid <- pcatsAPIclientR::staticGP(datafile="../../data/example1.csv",
-        outcome="Y",
-        treatment="A",
-        x.explanatory="X",
-        x.confounding="X",
-        burn.num=500, mcmc.num=500,
-        outcome.type="Continuous",
-        method="GP",
-        tr.type="Discrete",
-        pr.values="0,1,2")
+                                   outcome="Y",
+                                   treatment="A",
+                                   time="Time",
+                                   x.explanatory="X",
+                                   x.confounding="X",
+                                   burn.num=500, mcmc.num=500,
+                                   outcome.type="Continuous",
+                                   method="GP",
+                                   tr.type="Discrete",
+                                   c.margin="0,1,2")
 
 cat(paste0("JobID: ",jobid,"\n"))
 
