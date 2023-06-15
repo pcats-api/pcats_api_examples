8ea7ded2976fd4c679d25650f5fa9143789a4507
chen-chen130 <chen_chen130@hotmail.com>
2023-06-15 12:56:12 -0400

    Update pcats_api_example_manuscript_5.R

--- R/pcatsAPIclientR/pcats_api_example_manuscript_5.R
+++ R/pcatsAPIclientR/pcats_api_example_manuscript_5.R
@@ -3,21 +3,23 @@ library(pcatsAPIclientR)
 #example 5
 
 jobid <- pcatsAPIclientR::dynamicGP(datafile="../../data/example5.csv",
-        stg1.outcome="L1",
-        stg1.treatment="A1",
-        stg1.x.explanatory="U0",
-        stg1.x.confounding="U0",
-        stg1.outcome.type="Discrete",
-        stg1.tr.type="Discrete",
-        stg2.outcome="Y",
-        stg2.treatment="A2",
-        stg2.x.explanatory="U0,L1",
-        stg2.x.confounding="U0,L1",
-        stg2.outcome.type="Continuous", 
-        stg2.tr.type="Discrete",
-        burn.num=500,
-        mcmc.num=500,
-        method="BART")
+                                    stg1.outcome="L1",
+                                    stg1.treatment="A1",
+                                    stg1.time="Time1",
+                                    stg1.x.explanatory="U0",
+                                    stg1.x.confounding="U0",
+                                    stg1.outcome.type="Discrete",
+                                    stg1.tr.type="Discrete",
+                                    stg2.outcome="Y",
+                                    stg2.treatment="A2",
+                                    stg2.time="Time2",
+                                    stg2.x.explanatory="U0,L1",
+                                    stg2.x.confounding="U0,L1",
+                                    stg2.outcome.type="Continuous", 
+                                    stg2.tr.type="Discrete",
+                                    burn.num=500,
+                                    mcmc.num=500,
+                                    method="BART")
 
 cat(paste0("JobID: ",jobid,"\n"))
 
