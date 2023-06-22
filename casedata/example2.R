7288a5f732ee853e1ab1c99206a0c6fbebfed34c
chen-chen130 <chen_chen130@hotmail.com>
2023-06-22 15:40:40 -0400

    Update example2.R

--- casedata/example2.R
+++ casedata/example2.R
@@ -2,28 +2,28 @@ library(pcatsAPIclientR)
 
 download.file("https://github.com/pcats-api/pcats_api_examples/raw/main/casedata/example2.csv", destfile="example2.csv")
 
-download.file("https://github.com/pcats-api/pcats_api_examples/raw/main/casedata/example2_midata.csv", destfile="example2_midata.csv")
-
 jobid <- pcatsAPIclientR::dynamicGP(datafile="example2.csv",
                                     stg1.outcome='BMI1',
                                     stg1.treatment='A0',
-                                    stg1.x.explanatory="MET,Gender,BMI0,AGE,Obesity,time1",
-                                    stg1.x.confounding="BMI0,AGE,time1",
+                                    stg1.x.explanatory="MET,Gender,BMI0,AGE,Obesity",
+                                    stg1.x.confounding="BMI0,AGE",
                                     stg1.outcome.type='Continuous',
+                                    stg1.time = "time1",
+                                    stg1.time.value = 90,                                    
                                     stg2.outcome='BMI2',
                                     stg2.treatment='A1',
-                                    stg2.x.explanatory="MET,Gender,BMI0,AGE,Obesity,time1
-                                                       ,time2,BMI1",
-                                    stg2.x.confounding="BMI0,AGE,time1,time2,BMI1", 
+                                    stg2.x.explanatory="MET,Gender,BMI0,AGE,Obesity,BMI1",
+                                    stg2.x.confounding="BMI0,AGE,BMI1", 
                                     stg2.tr2.hte="BMI1",
                                     stg2.outcome.type='Continuous',
+                                    stg2.time = "time2",
+                                    stg2.time.value = 180,                                      
                                     burn.num=500,
                                     mcmc.num=500,
                                     stg1.tr.type = 'Discrete',
                                     stg2.tr.type = 'Discrete',
                                     method='BART',
-                                    x.categorical="MET,Gender,Obesity",
-                                    mi.datafile="example2_midata.csv")
+                                    x.categorical="MET,Gender,Obesity")
 
 cat(paste0("JobID: ",jobid,"\n"))
 
