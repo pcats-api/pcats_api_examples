f398a0d23d60eea3c133c4b65fc0da0efc8c740d
chen-chen130 <chen_chen130@hotmail.com>
2023-06-22 15:38:54 -0400

    Update example1.R

--- casedata/example1.R
+++ casedata/example1.R
@@ -6,28 +6,30 @@ download.file("https://github.com/pcats-api/pcats_api_examples/raw/main/casedata
 #Step 1: submit a request 
 jobid <- pcatsAPIclientR::staticGP(datafile="example1.csv",
                                    outcome="Jadas6",
-                                   treatment="treatment_group,diffvisit", 
+                                   treatment="treatment_group", 
                                    #specify the prognostic variables W
                                    x.explanatory="age,Female,chaq_score,RF_pos,private
                                                  ,Jadas0,timediag",
                                    #specify the confounders V
                                    x.confounding="age,Jadas0,chaq_score,timediag",
                             #add the term if you think the treatment effect is dfferent in RF_pos group
-                                   tr.hte="RF_pos",            
-                                   tr2.values=180,
+                                   tr.hte="RF_pos",   
+                                   #specify the time variable,
+                                   time="diffvisit",
+                                   #specify the time value used to calculate the ATE
+                                   time.value=180,
                                    burn.num=500, mcmc.num=500,
                                    #specify the type of outcome
                                    outcome.type="Continuous",
                                    method="GP",
                                    #specify the type of treatment
                                    tr.type="Discrete",
-                                   tr2.type = "Continuous",
                                    outcome.lb=0,
                                    outcome.ub=40,
                                    outcome.bound_censor="bounded",
                                    x.categorical="Female,RF_pos,private",
                                    #specify the value c used to calculate PrTE
-                                   pr.values="5",
+                                   c.margin="0,1",
                                    mi.datafile="example1_midata.csv")
 #retrieve the job id
 cat(paste0("JobID: ",jobid,"\n"))
@@ -47,9 +49,9 @@ if (status=="Done") {
 # CATE
 jobidcate <- pcatsAPIclientR::staticGP.cate(jobid=jobid,
                                             x="RF_pos",
-                                            control.tr="0,180",
-                                            treat.tr="1,180",
-                                            pr.values="5")
+                                            control.tr="1",
+                                            treat.tr="0",
+                                            c.margin="0,1")
 
 status <- pcatsAPIclientR::wait_for_result(jobidcate)
 
